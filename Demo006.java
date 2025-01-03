9e084f5e73b300feec8de719bd5c5054333805ec
BejugamRajkumar <rajkumarbejugam8727@gmail.com>
2025-01-03 11:07:27 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+package Core_Java;
+public class Demo006 {
+    public static void main(String[] args) {
+        String name = "Rajkumar";
+        String pwd = "Raj@87";
+        while ((name == "Rajkumar") && (pwd == "Raj@87")){
+            System.out.println("Hello Rajkumar welcome to mallareddy");
+                break;
+            }
+         }
+    }
