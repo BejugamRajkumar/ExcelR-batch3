1db5eaf6729004f6467ee3078e6646613d01e2f5
BejugamRajkumar <rajkumarbejugam8727@gmail.com>
2025-02-01 17:11:04 +0530

    Username and Password

---
+++ Demo006.java
@@ -0,0 +1,22 @@
+package Core_Java;
+
+import java.util.*;
+
+class Demo006{
+    public static void main(String[] args) {
+        boolean a = true;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter username: ");
+            String id = sc.next();
+            System.out.print("Enter password: ");
+            String pass = sc.next();
+            if((id.equals("Rajkumar")) && (pass.equals("Rajkumar_123"))){
+                System.out.println("Hello Rajkumar, You are welcome ");
+                break;
+            }else{
+                System.out.println("credentials does not match. Please try again.");
+            }
+        }
+    }
+}
